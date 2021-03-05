d96647d5d53fc0e75c6f3aee555afa2bbf42d325
Eric Parsonnet <e.parsonnet@berkeley.edu>
2021-03-05 10:45:56 -0800

    adding area and diameter to common_name_mapper

--- analysis/FE_switching/main.py
+++ analysis/FE_switching/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 __all__ = ('common_name_mapper',)
 
 
@@ -17,13 +19,25 @@ def common_name_mapper(fname):
 	string = fname.replace('.csv','')
 	spl = string.split('_')
 	
-	ordered_keys = ['identifier', 
-					'pulsewidth_ns', 
-					'delay_ns',
-					''
-				   ]
+	area_diameter_dict = {
+		'{}um'.format(np.round(5 + d*4.5, 1)).replace('0','').replace('.',''):
+			(np.pi*((np.round(5 + d*4.5, 1))/2)**2, np.round(5 + d*4.5, 1)) for d in range(9)
+	}
+
+	area_diameter_dict.update(
+		{
+			'125um':(np.pi*(12.5/2)**2, 12.5),
+			'25um':(np.pi*(25/2)**2, 25),
+			'50um':(np.pi*(50/2)**2, 50),
+		}
+	)
+
+	area, diameter = area_diameter_dict[spl[0].split('um')[0] + 'um']
+
 	out = dict({
 		'identifier':spl[0],
+		'area': area,
+		'diameter': diameter,
 		'pulsewidth_ns':float(spl[1])*1e9,
 		'delay_ns':float(spl[2])*1e9,
 		'high_voltage_v':float(spl[3].replace('V','').replace('x','.')),
