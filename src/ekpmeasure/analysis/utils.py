d8e49836459f03d166cd6c51994a65c8dfd68407
Eric Parsonnet <e.parsonnet@berkeley.edu>
2021-10-25 21:32:11 -0700

    deprecations complete

--- src/ekpmeasure/analysis/utils.py
+++ src/ekpmeasure/analysis/utils.py
@@ -6,7 +6,7 @@ import warnings
 from .core import Dataset, Data
 from .core import _convert_ITP_to_path_to_index
 
-__all__ = ('merge','merge_Datasets', 'merge_Datas')
+__all__ = ('merge_Datasets', 'merge_Datas')
 
 def merge_Datas(datas):
 	"""Merge data.
@@ -70,37 +70,3 @@ def merge_Datasets(datasets):
 	path = _convert_ITP_to_path_to_index(new_path)
 	
 	return Dataset(path,new_df,readfileby=readfileby)
-
-def merge(datasets):
-	"""Merge datasets.
-
-	args:
-		datasets (iter of Dataset): Iterable of Dataset objects to merge.
-
-	returns:
-		(Dataset): Merged dataset.
-
-	"""
-	warnings.showwarning('merge() is deprecated. use merge_Datasets() instead', DeprecationWarning, '', 0,)
-	
-	if not hasattr(datasets, '__iter__'):
-		raise TypeError('datasets is not iterable.')
-		
-	columns = datasets[0].columns
-	for dset in datasets[1:]:
-		if len(columns) != len(dset.columns):
-			raise ValueError('supplied datasets do not all have the same columns!')
-		if (columns != dset.columns).all():
-			raise ValueError('supplied datasets do not all have the same columns!')
-	
-	for i, dset in enumerate(datasets):
-		if i == 0:
-			new_path = dset.index_to_path
-			new_df = pd.DataFrame(dset.meta_data)
-		else:
-			new_path = pd.concat((new_path, dset.index_to_path), ignore_index = True)
-			new_df = pd.concat((new_df, pd.DataFrame(dset.meta_data)), ignore_index = True)
-			
-	path = _convert_ITP_to_path_to_index(new_path)
-	
-	return Dataset(path,new_df)
