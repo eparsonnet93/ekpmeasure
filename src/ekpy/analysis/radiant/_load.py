5233cf59d255012d7dac28552800575b42733c62
Eric Parsonnet <e.parsonnet@berkeley.edu>
2022-03-09 17:44:38 -0800

    adding read_radiant_txt which aims to be able to read any radiant datafile

--- src/ekpy/analysis/radiant/_load.py
+++ src/ekpy/analysis/radiant/_load.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-__all__ = ('load_radiant_loop_from_text_file','read_loop_txt')
+__all__ = ('load_radiant_loop_from_text_file','read_loop_txt', 'read_radiant_txt')
 
 def load_radiant_loop_from_text_file(file, measured_value='Charge', return_meta_data=False, delimiter=','):
     raise NameError('"load_radiant_loop_from_text_file" is deprecated. Please use "read_loop_txt"')
@@ -13,6 +13,7 @@ def read_loop_txt(file, measured_value='Charge', return_meta_data=False, delimit
         file (str): Filename and path. 
         measured_value (str): 'Charge' or 'Polarization' or 'Current'. Current is returned in mA
         return_meta_data (bool): Return meta data (dict)
+        delimiter (str): Delimiter for data file
 
     returns:
         (pandas.DataFrame, (dict)): Data, Optional: (meta data)
@@ -87,3 +88,161 @@ def read_loop_txt(file, measured_value='Charge', return_meta_data=False, delimit
         return out, meta_data
         
     return out
+
+def read_radiant_txt(file, measured_charge=True, return_meta_data=False, delimiter=','):
+    """Load a radiant data file (typically as generated from quicklook functions). If retrieving a hysteresis file, one would typically use 'True' for measured_charge unless one accurately measured capacitor area and input correctly into the Radiant UI.
+
+    args:
+        file (str): Filename and path. 
+        measured_hysteresis_value (bool): *For hysteresis files only*, wheter to return hysteresis data in charge, not polarization
+        return_meta_data (bool): Return meta data (dict)
+        delimiter (str): Delimiter for data file
+
+    returns:
+        (pandas.DataFrame, (dict)): Data, Optional: (meta data)
+    """
+    supported_types = {'pund', 'hysteresis', 'simplepulse', 'currentloop'}
+
+    lines = _get_lines(file)
+    # find where the data starts
+    try:
+        where_data = lines.index('Data')
+    except ValueError:
+        raise ValueError('Unable to find any data i.e. "Data" in file "{}". Please confirm you are using the correct file'.format(file))
+
+    meta_datalines = lines[:where_data]
+    datalines = lines[where_data+1:]
+    
+    data_file_type = meta_datalines.pop(0).lower()
+    if data_file_type not in supported_types:
+        raise ValueError('Data type "{}" not yet supported. Must be of type {}'.format(data_file_type, supported_types))
+
+    # sometimes radiant puts multiple lines that claim to be the start of Data, sometimes its called "data", sometimes "valid data"
+    to_remove = []
+    for index, line in enumerate(datalines):
+        if 'Data' in line:
+            to_remove.append(index)
+
+    # now we have isolated the actual data
+    try:
+        datalines = datalines[max(to_remove)+1:]
+    except ValueError:
+        pass
+
+    # build the meta data
+    meta_data = {}
+    for line in meta_datalines:
+        if ':{}{}'.format(delimiter, delimiter) in line: # as is the case sometimes, but not always!!
+            spl = line.split(':{}{}'.format(delimiter, delimiter))
+        elif ':{}'.format(delimiter) in line: # as is the case sometimes, but not always!!
+            spl = line.split(':{}'.format(delimiter))
+        elif ':' in line:
+            spl = line.split(':')
+        else:
+            continue
+        meta_data.update({spl[0]:spl[1]})
+
+
+    # build the data
+    if data_file_type == 'hysteresis':
+        data = _hystersis_parser(datalines, delimiter=delimiter)
+        if measured_charge:
+            try:
+                data['MeasuredPolarization'] = data['MeasuredPolarization']*float(meta_data['SampleArea(cm2)'])*1e6 #to convert from uC to pC
+            except KeyError:
+                raise KeyError('no key "MeasuredPolarization", is this a current loop? use measured_value="current"')
+            data.rename(columns = {'MeasuredPolarization':'MeasuredCharge(pC)'}, inplace = True)
+        else:
+            data.rename(columns = {'MeasuredPolarization':'MeasuredPolarization(uC/cm2)'}, inplace = True)
+    elif data_file_type == 'currentloop':
+        data = _hystersis_parser(datalines, delimiter=delimiter)
+    elif data_file_type == 'pund':
+        data = _pund_parser(datalines, delimiter=delimiter)
+    elif data_file_type == 'simplepulse':
+        data = _pund_parser(datalines, delimiter=delimiter)
+    else:
+        import pdb; pdb.set_trace()
+        
+    if return_meta_data:
+        return data, meta_data
+        
+    return data
+
+def _get_lines(file):
+    # do a bunch of crap to make their data readable
+    with open(file, 'rb') as f:
+        lines = f.readlines()
+
+    newlines = []
+
+    for i, line in enumerate(lines):
+        #radiant's file structure is the worst
+        newlines.append(str(line.decode('windows-1252').replace(
+            '»', ''
+        ).replace(
+            '«', ''
+        ).replace(
+            ' ', ''
+        ).replace(
+            '\r', ''
+        ).replace(
+            '\n', ''
+        ).replace(
+            'µ', 'u'
+        ).replace(
+            'â', ''
+        ).replace('Â', '')
+                           ))
+
+    lines = newlines
+    
+    # loop to remove blank lines
+    remove = True
+    while remove:
+        try:
+            lines.remove('')
+        except ValueError:
+            remove = False
+            
+    return lines
+
+
+def _hystersis_parser(datalines, delimiter=','):
+    # get column headers
+    colnames = datalines.pop(0).split(delimiter) 
+
+    if not len(colnames)>1:
+        raise ValueError('Unable to determine data column names. Is your delimter correct? It is currently set to "{}"'.format(delimiter))
+
+    data = {col:[] for col in colnames}
+    for ijk, line in enumerate(datalines):       
+        try:
+            spl = line.split(delimiter)
+            if len(spl) != len(colnames):
+                continue
+            for col, val in zip(colnames, spl):
+                data[col].append(float(val.replace(',', '')))
+
+        except Exception as err:
+            if ijk == len(datalines)-1:
+                continue
+            else: 
+                raise err
+
+    data = pd.DataFrame(data)
+    return data
+
+def _pund_parser(datalines, delimiter=','):
+    data = {}
+    for ijk, line in enumerate(datalines):
+        try:
+            spl = line.split(':')
+            if len(spl)!= 2:
+                continue
+            data.update({spl[0]:[float(spl[1].replace(',', ''))]})
+        except Exception as err:
+            if ijk == len(datalines)-1:
+                continue
+            else: 
+                raise err
+    return pd.DataFrame(data)
