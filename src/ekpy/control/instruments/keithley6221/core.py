e853b7c9fc12ee1f0f88d876909db45c0417f9d6
ElPsyKurisu <84881414+ElPsyKurisu@users.noreply.github.com>
2022-07-13 15:31:02 -0700

    Updated core.py
    Generalized the set output waveform function to allow for ramp, sin, and square waves in a singular function. Incorporated dc offset as well.

--- src/ekpy/control/instruments/keithley6221/core.py
+++ src/ekpy/control/instruments/keithley6221/core.py
@@ -1,3 +1,4 @@
+import wave
 import numpy as np
 from .. import misc
 from ....utils import get_number_and_suffix
@@ -15,13 +16,16 @@ def restore(current_source):
     current_source.write("*rst")
     return
 
-def set_output_sin(current_source, frequency, amplitude, compliance = 1.1):
-    """Set the current source to output a sin waveform with specified amplitude and frequency. Does not start the current source.
+def set_output_waveform(current_source, frequency, amplitude, waveform='sin', offset=0, compliance = 1.1):
+    """Set the current source to output a specified waveform with specified amplitude and frequency.
+    Does not start the current source. By default a sine wave with a 0mA DC offset.
 
     args:
         current_source (pyvisa.resources.gpib.GPIBInstrument): Keithley 6221
+        waveform(str): Waveform. Allowed values: 'sine', 'square', 'ramp'
         frequency (str): Frequency. Allowed suffix 'khz', 'hz'.
         amplitude (str): Amplitude. Allowed suffix 'ua', 'ma'.
+        offset (str): Offset. Allowed suffix 'ua', 'ma'.
         compliance (float): Compliance in V. 
 
 
@@ -34,19 +38,29 @@ def set_output_sin(current_source, frequency, amplitude, compliance = 1.1):
     amp_number, amp_suffix = get_number_and_suffix(amplitude)
     if amp_suffix not in set(misc.current_amp_mapper.keys()):
         raise KeyError('amplitude suffix {} is not allowed. (allowed are ma and ua)'.format(amp_suffix))
-        
+    offs_number, offs_suffix = get_number_and_suffix(offset)
+    if offs_suffix not in set(misc.current_amp_mapper.keys()):
+        raise KeyError('Offset suffix {} is not allowed. (allowed are {})'.format(offs_suffix,set(misc.current_amp_mapper.keys())))
+    offs = str(offs_number) + misc.current_amp_mapper[offs_suffix]    
     freq = str(freq_number) + misc.freq_mapper[freq_suffix]
     amp = str(amp_number) + misc.current_amp_mapper[amp_suffix]
-
+    waveform = waveform.lower()
+    if waveform == 'sine' or waveform == 'sin':
+        wf = 'SIN'
+    if waveform == 'square' or waveform == 'squ':
+        wf = 'SQU'
+    if waveform == 'ramp':
+        wf = "RAMP"
     command = """
-    SOUR:WAVE:FUNC SIN
+    SOUR:WAVE:FUNC {}
     SOUR:WAVE:FREQ {}
     SOUR:WAVE:AMPL {}
+    SOUR:WAVE:OFFS {}
     SOUR:WAVE:PMAR:STAT ON
     SOUR:WAVE:PMAR:OLIN 4
     SOUR:WAVE:RANG BEST
     SOUR:CURR:COMP {}
-    """.format(freq, amp, compliance)
+    """.format(wf, freq, amp, offs, compliance)
     current_source.write(command)
     return
 
